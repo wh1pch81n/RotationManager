95c40350bc94008382f5f886f3da631a2f4f96af
Derrick Ho <derrickho@derricks-mbp.fios-router.home>
2017-06-30 20:40:24 -0400

    Initial Commit

---
+++ RotationManager/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RotationManager
+//
+//  Created by Derrick Ho on 6/30/17.
+//  Copyright © 2017 Derrick Ho. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
